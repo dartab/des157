fbb7714707a35cce357361ad8fd8b7117e85c005
Daria Taback <dptaback@ucdavis.edu>
2017-03-16 14:16:27 -0700

    dfg

--- project/process/proto3/script.js
+++ project/process/proto3/script.js
@@ -22,15 +22,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
     // click on alienhead for info box
     alienhead.addEventListener('click', function() {
         aboutalienhead.style.display = "block";
-        close.style.cursor = "pointer";
+        aboutalienbody.style.display = "none";
+        aboutalienhead.style.cursor = "pointer";
     });
 
     // alienhead info box that displays on click
     document.getElementById('aboutalienhead');
-    var close = document.getElementById('close');
+    // var close = document.getElementById('close');
 
 
-    close.addEventListener('click', function() {
+    aboutalienhead.addEventListener('click', function() {
         aboutalienhead.style.display = "none";
 
       });
@@ -58,15 +59,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
       // click on alienbody for info box
       alienbody.addEventListener('click', function() {
           aboutalienbody.style.display = "block";
-          close1.style.cursor = "pointer";
+          aboutalienhead.style.display = "none";
+          aboutalienbody.style.cursor = "pointer";
       });
 
       // alienbody info box that displays on click
       document.getElementById('aboutalienbody');
-      var close1 = document.getElementById('close1');
+      // var close1 = document.getElementById('close1');
 
 
-      close1.addEventListener('click', function() {
+      aboutalienbody.addEventListener('click', function() {
           aboutalienbody.style.display = "none";
 
         });
